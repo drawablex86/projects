ca4a3add01a47708aa8b57239b9527c9d6bb28d4
rahulrajeeeev <rahulrajeev1975@gmail.com>
2025-03-01 06:46:22 +0530

    Youtube easy embed

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,20 @@ module.exports = function(eleventyConfig) {
 
 	});
 
+//Short code to easily add youtube embeds to md file. Ex {% youtube "qGs8-xBForM" %}
+  // Add YouTube shortcode before other shortcodes
+  eleventyConfig.addShortcode("youtube", function(videoId) {
+    return `<div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; width: 100%; margin-bottom: 1rem;">
+      <iframe 
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" 
+        src="https://www.youtube.com/embed/${videoId}" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+      </iframe>
+    </div>`;
+  });
+
   // Shortcode to generate a responsive project image
   eleventyConfig.addShortcode("generateImage", async function(params) {
 
